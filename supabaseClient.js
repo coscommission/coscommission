4c6ec2bc9f463a84fab50e9f94f0ec425103bf8b
lancy2727 <xinyuan.wang@mail.utoronto.ca>
2026-01-03 20:01:08 -0500

    sign up debug

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,4 +2,8 @@
 const SUPABASE_URL = "https://yqxhungbzarfrccpbohy.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxeGh1bmdiemFyZnJjY3Bib2h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczMTc2MjQsImV4cCI6MjA4Mjg5MzYyNH0.ySCcPDOIGx-XJ0GIHhFzIAQtbhlZTj6rBzXk1pseaY8";
 
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY,
+  { auth: { detectSessionInUrl: true } }
+);

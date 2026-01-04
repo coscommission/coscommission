87ecde4d4c13155d35e2aabf10f9271ca7ce901d
lancy2727 <xinyuan.wang@mail.utoronto.ca>
2026-01-03 19:26:30 -0500

    Beta signup flow + magic link auth

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 // supabaseClient.js
 const SUPABASE_URL = "https://yqxhungbzarfrccpbohy.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_jE-PHcd3uUYTR0MXPI1V2A_OPEmKVnu";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxeGh1bmdiemFyZnJjY3Bib2h5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczMTc2MjQsImV4cCI6MjA4Mjg5MzYyNH0.ySCcPDOIGx-XJ0GIHhFzIAQtbhlZTj6rBzXk1pseaY8";
 
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
